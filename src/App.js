9c449060f93ee8dd35774c66073c3561b4c2ddae
= <=>
2020-04-20 21:05:29 +0530

    React Componests

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuCompo';
 import './App.css';
+import {DISHES} from './shared/dishes'
 
 class App extends Component {
+    constructor(props){
+      super(props);
+
+      this.state = {
+        dishes: DISHES
+      };
+    }
+
   render() {
     return(
-      <div className="App">
+      <div>
       <Navbar dark color="primary">
       <div className="container">
       <NavbarBrand href="/">Ristornate con fusion</NavbarBrand>
       </div>
       </Navbar>
+      <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
