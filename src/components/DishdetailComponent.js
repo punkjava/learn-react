05641ab4fdfe4e3ecd8ce68ed8869a656dfa2055
= <=>
2020-05-03 17:14:26 +0530

    cross-fetch and error handling

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,8 @@ import { Card, CardImg, Breadcrumb,BreadcrumbItem, CardText, CardBody,CardTitle,
           Button,Modal,ModalBody, Row,Label,Col,ModalHeader} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
-import {Loading} from './LoadingComponent'
+import {Loading} from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -16,7 +17,7 @@ function RenderDish({dish}){
     return(
       // <div className = "col-12 col-md-5 m-1">
       <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
+        <CardImg top src={baseUrl +dish.image} alt={dish.name} />
         <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
